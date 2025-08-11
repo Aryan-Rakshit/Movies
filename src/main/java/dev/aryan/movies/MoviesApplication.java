9612eec61cf2c2baafe029dc4c6ac86063310145
Aryan_Rakshit <rakshita@purdue.edu>
2025-08-11 17:01:39 -0400

    .env.example

--- src/main/java/dev/aryan/movies/MoviesApplication.java
+++ src/main/java/dev/aryan/movies/MoviesApplication.java
@@ -3,6 +3,7 @@ package dev.aryan.movies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class MoviesApplication {
 
@@ -11,3 +12,4 @@ public class MoviesApplication {
 	}
 
 }
+
