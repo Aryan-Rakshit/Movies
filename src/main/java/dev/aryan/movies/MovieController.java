043e738a9b0ad2f6e5f9339a58191f98bae41bcb
Aryan_Rakshit <rakshita@purdue.edu>
2025-08-15 21:45:51 -0400

    accesses movie by ImdbId

--- src/main/java/dev/aryan/movies/MovieController.java
+++ src/main/java/dev/aryan/movies/MovieController.java
@@ -19,7 +19,7 @@ import java.util.Optional;
 public class MovieController {
 
     @Autowired
-    private MovieService movieService; //new
+    private MovieService movieService;
 
     @GetMapping
     public ResponseEntity<List<Movie>> getAllMovies() {
