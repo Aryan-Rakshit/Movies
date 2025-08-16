5376e72d55640914c0d07e2d23c302a1ba1589eb
Aryan_Rakshit <rakshita@purdue.edu>
2025-08-15 21:43:41 -0400

    accesses movie by ImdbId

--- src/main/java/dev/aryan/movies/MovieService.java
+++ src/main/java/dev/aryan/movies/MovieService.java
@@ -1,4 +1,24 @@
 package dev.aryan.movies;
 
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+//Uses repository class and talks to the database and gets the list of movies and returns to the api layer
+@Service
 public class MovieService {
+    //database access methods
+    @Autowired
+    private MovieRepository movieRepository;
+
+    public List<Movie> allMovies() {
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(String imdbId) {
+        return movieRepository.findMovieByImdbId(imdbId);
+    }
 }
